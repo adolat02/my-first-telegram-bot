3e93e6a9d1b4a48a047c1952668d6f55d1fe93db
Your Name <you@example.com>
2025-01-21 16:45:16 +0500

    this is my first telegram bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7423518960:AAFRKlp4RSxzFVilRBi0j8Krmu5lOuCZyZI")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.reply("Hello!")
+
+@dp.message(Command("lang"))
+async def select_lang(msg: types.Message):
+    await msg.answer("Select your language")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
